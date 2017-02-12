f9ce353b8d84a7acce877bd4c1159d2984235d75
Andreas Holstenson <a@holstenson.se>
2017-02-12 09:03:16 +0100

    Set boosts in UserQuery

--- silo-search-api/src/main/java/se/l4/silo/search/query/UserQuery.java
+++ silo-search-api/src/main/java/se/l4/silo/search/query/UserQuery.java
@@ -6,15 +6,21 @@ public class UserQuery<R>
 	extends AbstractQueryPart<R>
 {
 	private final String[] fields;
+	private float[] boosts;
 
 	public UserQuery(String... fields)
 	{
 		this.fields = fields;
+		this.boosts = new float[fields.length];
+		for(int i=0, n=boosts.length; i<n; i++)
+		{
+			boosts[i] = 1;
+		}
 	}
 	
 	public R text(String query)
 	{
-		receiver.addQuery(new QueryItem("user", new UserQueryData(fields, query)));
+		receiver.addQuery(new QueryItem("user", new UserQueryData(fields, boosts, query)));
 		return parent;
 	}
 }
