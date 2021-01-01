f7a26fcb1f16fc53823e28b9b4650321025d6aa1
Andreas Holstenson <a@holstenson.se>
2021-01-01 17:28:17 +0100

    test(engine): Additional tests for entity storing

--- silo-engine/src/test/java/se/l4/silo/engine/internal/EntityTest.java
+++ silo-engine/src/test/java/se/l4/silo/engine/internal/EntityTest.java
@@ -1,12 +1,14 @@
 package se.l4.silo.engine.internal;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
+import reactor.core.publisher.Flux;
 import se.l4.exobytes.Serializers;
 import se.l4.silo.Entity;
 import se.l4.silo.Transaction;
@@ -100,6 +102,78 @@ public class EntityTest
 		assertThat(
 			entity().get(1).blockOptional(),
 			is(Optional.empty())
+		);
+	}
+
+	@Test
+	public void storeAndOverwrite()
+	{
+		TestUserData o1 = new TestUserData(1, "V1", 20, true);
+
+		entity().store(o1).block();
+
+		TestUserData d1 = entity().get(1).block();
+		assertThat(d1, is(o1));
+
+		TestUserData o2 = new TestUserData(1, "V2", 20, true);
+
+		entity().store(o2).block();
+
+		TestUserData d2 = entity().get(1).block();
+		assertThat(d2, is(o2));
+	}
+
+	@Test
+	public void overwriteInTransaction()
+	{
+		TestUserData o1 = new TestUserData(1, "V1", 20, true);
+		TestUserData o2 = new TestUserData(1, "V2", 20, true);
+
+		instance().inTransaction(() -> {
+			entity().store(o1).block();
+			entity().store(o2).block();
+		}).block();
+
+		TestUserData d2 = entity().get(1).block();
+		assertThat(d2, is(o2));
+	}
+
+	@Test
+	public void storeFluxNoTransaction()
+	{
+		Flux.range(1, 1000)
+			.map(i -> new TestUserData(i, "V" + i, i % 40, i % 2 == 0))
+			.flatMap(entity()::store)
+			.blockLast();
+
+		assertThat(
+			entity().get(1).block(),
+			notNullValue()
+		);
+
+		assertThat(
+			entity().get(1000).block(),
+			notNullValue()
+		);
+	}
+
+	@Test
+	public void storeFluxTransactional()
+	{
+		instance().transactional(
+			Flux.range(1, 1000)
+				.map(i -> new TestUserData(i, "V" + i, i % 40, i % 2 == 0))
+				.flatMap(entity()::store)
+		).blockLast();
+
+		assertThat(
+			entity().get(1).block(),
+			notNullValue()
+		);
+
+		assertThat(
+			entity().get(1000).block(),
+			notNullValue()
 		);
 	}
 }
